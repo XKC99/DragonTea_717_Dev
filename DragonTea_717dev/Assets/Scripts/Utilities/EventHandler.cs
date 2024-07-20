a238e10175a16c5b82a2fdf0e191134a5257bcab
Starry Tea <1556403452@qq.com>
2024-07-20 16:00:16 +0800

    不知道为什么没声音
    1

--- DragonTea_717dev/Assets/Scripts/Utilities/EventHandler.cs
+++ DragonTea_717dev/Assets/Scripts/Utilities/EventHandler.cs
@@ -3,8 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class EventHandler 
+public static class EventHandler  //事件控制中心
 {
-    public static event Action<ItemDetails,int>UpdateUIEvent;
-   
+   public static event Action BoxCollision;  //盒子碰撞事件。事件命名方法。
+   public static void CallBoxOnCollision() //用于触发盒子碰撞事件。当有方法订阅该事件时，会调用订阅的方法
+   { 
+    //运行CallBoxONCollision后如果存在OnBoxCollision事件，则调用OnBoxCollision事件。
+      BoxCollision?.Invoke();
+   }
+
+  
+
+
 }

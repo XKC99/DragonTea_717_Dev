176499e84f30c797e786491a03571eba831d295c
Starry Tea <1556403452@qq.com>
2024-07-28 21:07:30 +0800

    item框架梳理
    1

--- DragonTea_717dev/Assets/Scripts/Items/ItemLogic.cs
+++ DragonTea_717dev/Assets/Scripts/Items/ItemLogic.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class ItemLogic : MonoBehaviour,IFireHitandHeal  //接口命名往往以I开头
 {
+
+   protected GameObject player;
+   protected virtual void Awake()
+   {
+      player=GameObject.FindGameObjectWithTag("Player");
+   }
+
+    public virtual bool Execute(Card card) 
+    {
+        switch(card.cardType)
+        {
+            case CardType.Fire:
+                Debug.Log("攻击");
+                player.GetComponent<PlayerController>().PlayerIsAttack();
+                return true;
+             case CardType.Heal:
+                 Debug.Log("治疗");
+                 return true;  //如果不想让这类牌发挥作用，返回false或者直接注释
+            case CardType.Fly:
+                Debug.Log("飞行");
+                return true;
+            case CardType.Fall:
+                Debug.Log("坠落");
+                return true;
+        }
+        return false;
+    }
+    
+
+
+
+
+
     protected virtual void OnCollider(string AudioName)
     {
         

b2c48146bf330153c452ea254934ef2a4f41e2aa
Starry Tea <1556403452@qq.com>
2024-07-29 12:02:27 +0800

    箱子即基本物体逻辑实现完毕
    1

--- DragonTea_717dev/Assets/Scripts/Items/ItemLogic.cs
+++ DragonTea_717dev/Assets/Scripts/Items/ItemLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,18 @@ public class ItemLogic : MonoBehaviour,IFireHitandHeal  //接口命名往往以I
 {
 
    protected GameObject player;
+   protected Rigidbody2D rb;
+   public float jumpForce=10f;
    protected virtual void Awake()
    {
       player=GameObject.FindGameObjectWithTag("Player");
+      rb=this.gameObject.GetComponent<Rigidbody2D>();
+      if(rb==null)
+      {
+        Debug.Log("当前对象不村子Rigidbody2D");
+        return;
+      }
+
    }
 
     public virtual bool Execute(Card card) 
@@ -19,11 +29,20 @@ public class ItemLogic : MonoBehaviour,IFireHitandHeal  //接口命名往往以I
                 Debug.Log("攻击");
                 player.GetComponent<PlayerController>().PlayerIsAttack();
                 return true;
-             case CardType.Heal:
-                 Debug.Log("治疗");
-                 return true;  //如果不想让这类牌发挥作用，返回false或者直接注释
+            case CardType.Heal:
+                Debug.Log("治疗");
+                player.GetComponent<PlayerController>().PlayerIsHeal();
+                return true;  //如果不想让这类牌发挥作用，返回false或者直接注释
             case CardType.Fly:
                 Debug.Log("飞行");
+                if(rb!=null)
+                {
+                    //rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse); //给一个向上的力
+                    rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+                }
+                else{
+                    Debug.Log("进行特殊处理"); 
+                }
                 return true;
             case CardType.Fall:
                 Debug.Log("坠落");
@@ -32,11 +51,6 @@ public class ItemLogic : MonoBehaviour,IFireHitandHeal  //接口命名往往以I
         return false;
     }
     
-
-
-
-
-
     protected virtual void OnCollider(string AudioName)
     {
         
@@ -57,4 +71,6 @@ public class ItemLogic : MonoBehaviour,IFireHitandHeal  //接口命名往往以I
     {
        Debug.Log("我被治疗了");
     }
+
+
 }
--- DragonTea_717dev/Assets/Scripts/Player/PlayerCollision.cs
+++ DragonTea_717dev/Assets/Scripts/Player/PlayerCollision.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using NodeCanvas.DialogueTrees;
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
+public class PlayerCollision : ItemLogic,ICardAffected 
 {
 
     private GameObject FNote;//靠近NPC显示的提示UI
     public DialogueTreeController npcDialogueTreeController;  //NPC身上的DialogueTreeController组件
+
+    public override bool Execute(Card card)
+    {
+        switch(card.cardType)
+        {
+            // case CardType.Fire:
+            //     Debug.Log("攻击");
+            //     //player.GetComponent<PlayerController>().PlayerIsAttack();
+            //     return true;
+            // case CardType.Heal:
+            //     Debug.Log("治疗");
+            //     //player.GetComponent<PlayerController>().PlayerIsHeal();
+            //     return true;  //如果不想让这类牌发挥作用，返回false或者直接注释
+            case CardType.Fly:
+                Debug.Log("飞行");
+                if(rb!=null)
+                {
+                    //rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse); //给一个向上的力
+                    rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+                }
+                else{
+                    Debug.Log("进行特殊处理"); 
+                }
+                return true;
+            case CardType.Fall:
+                Debug.Log("坠落");
+                return true;
+        }
+        return false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -31,7 +61,21 @@ public class PlayerCollision : MonoBehaviour
     {
         
     }
-   
+
+    //卡牌功能相关如下
+        //  if(other.gameObject.CompareTag("Card"))  //当卡牌拖拽到人物身上并释放时
+        //  {
+        //      other.gameObject.GetComponent<CardHandler>().SetExcuteTure(this);
+        //  }
+        // if(other.gameObject.tag == "Fire")  //当人物接触到攻击火球时
+        // {
+        //     OnFire();
+        //     Destroy(other.gameObject);
+        // }
+        // if(other.gameObject.tag == "Heal")  //当人物接触到治愈火球时
+        // {
+        //     Debug.Log("我被治愈了");
+        // }
    }
 
    private void OnTriggerExit2D(Collider2D other)
